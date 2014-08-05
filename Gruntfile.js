fc67d24f00f82325558a5ab608d61ba6df0f34bc
qisan <yimiqisan@gmail.com>
2014-08-05 17:59:59 +0800

    add ignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,18 @@
-// Generated on 2014-07-08 using generator-webapp 0.4.9
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
-
-    // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
-
-    // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
-    // Configurable paths
     var config = {
         app: 'static-src',
         dist: 'src/microsite/static'
     };
 
-    // Define the configuration for all the tasks
     grunt.initConfig({
 
-        // Project settings
         config: config,
 
-        // Watches files for changes and runs tasks based on the changed files
         watch: {
             bower: {
                 files: ['bower.json'],
