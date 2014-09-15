ec9fd959e7b1169895b420c8597e7d0f0043d8b2
qisanstudio <qisanstudio@gmail.com>
2014-09-15 20:14:11 +0800

    fix bug about db

--- src/microsite/__init__.py
+++ src/microsite/__init__.py
@@ -3,14 +3,11 @@
 
 from flask.ext.babelex import Babel
 from studio.core.flask.app import StudioFlask
-from studio.core.engines import db
 
 
 app = StudioFlask(__name__)
 
-db.init_app(app)
 Babel(app=app, default_locale='zh')
-
 
 with app.app_context():
     from microsite import views
